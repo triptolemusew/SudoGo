dc69a432b97dfe418daf5816779bc18433a6c957
triptolemusew <deneuve.crepes@gmail.com>
2018-01-14 04:11:46 +0800

    Generating a random 3x3 randombox 2D matrix

--- main.go
+++ main.go
@@ -1,49 +1,64 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
 
 func main() {
+	size := 9
 	rand.Seed(time.Now().UTC().UnixNano())
-	matrixCell := make([][]int, 9)
+	matrixCell := make([][]int, size)
 	for i := range matrixCell {
-		matrixCell[i] = make([]int, 9)
+		matrixCell[i] = make([]int, size)
 	}
 
+	sizeSqrt := int(math.Sqrt(float64(size)))
 	for i := 0; i < 3; i++ {
-		arr := generateRandomBox()
-		for x := 0; x < x+3; x++ {
-			for y := 0; y < y+3; y++ {
+		arr := generateRandomBox(size)
+		for x := 0; x < 3; x++ {
+			for y := 0; y < 3; y++ {
 				switch i {
 				case 0:
-					matrixCell[x][y] = arr[y]
+					matrixCell[x][y] = arr[x][y]
 				case 1:
-					x = 3
-					y = 3
-					matrixCell[x][y] = arr[y]
+					matrixCell[x+sizeSqrt][y+sizeSqrt] = arr[x][y]
 				case 2:
-					x = 6
-					y = 6
-					matrixCell[x][y] = arr[y]
+					matrixCell[x+(sizeSqrt*2)][y+(sizeSqrt*2)] = arr[x][y]
 				}
 			}
 		}
 	}
-
 	printMatrix(matrixCell)
 }
 
-func generateRandomBox() []int {
-	arr := make([]int, 9)
-	for i := 0; i < 9; i++ {
-		arr[i] = rand.Intn(10)
+func generateRandomBox(size int) [][]int {
+	arr := make([][]int, size)
+	for i := range arr {
+		arr[i] = make([]int, size)
+	}
+
+	uniqueArr := rand.Perm(9 - 1 + 1)
+	count := 0
+	for x := 0; x < 3; x++ {
+		for y := 0; y < 3; y++ {
+			if uniqueArr[count] == 0 {
+				uniqueArr[count] = 9
+			}
+			arr[x][y] = uniqueArr[count]
+			count++
+		}
 	}
 
 	return arr
 }
 
-func printMatrix([][]int) {
+func printMatrix(matrixCell [][]int) {
+	fmt.Println(matrixCell)
+}
+
+func checkUnique(matrixCell [][]int, size int) {
 
 }
